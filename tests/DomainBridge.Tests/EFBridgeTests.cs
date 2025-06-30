96766a2bf5eb66b54f8b91f6f980dd489503ef78
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-06-30 15:32:36 +0100

    EFBridgeTests

---
+++ tests/DomainBridge.Tests/EFBridgeTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace DomainBridge.Tests;
+
+public class EFBridgeTests
+{
+    [DomainBridge(typeof(DbContext))]
+    public partial class EFBridge;
+
+    [Test]
+    public void Test()
+    {
+        var bridge = Tests.EFBridge.Create(() => null);
+    }
+}
---
+++ tests/DomainBridge.Tests/PlaywrightBridgeTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using Microsoft.Playwright;
+
+namespace DomainBridge.Tests;
+
+public class PlaywrightBridgeTests
+{
+    [DomainBridge(typeof(Form))]
+    public partial class FormBridge;
+
+    [Test]
+    public void Test()
+    {
+        var bridge = Tests.FormBridge.Create(() => null);
+    }
+}
